445e054b977ca7825a0939bb1a64a927c0453bf1
sonu kumar <gautamrocky909621@gmail.com>
2025-11-09 16:03:52 +0530

    apiclient has been changed to use the deployed backend link

--- src/api/apiClient.jsx
+++ src/api/apiClient.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:9090/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL ;
+// || 'http://localhost:9090/api'
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 30000, // Increased timeout to 30 seconds
+  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
   },
 });
 
